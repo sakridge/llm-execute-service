88eb7e64fbffeae0daa91aa8829703efba805c5f
Stephen Akridge <sakridge@gmail.com>
2023-08-23 15:54:17 -0700

    Load dynamic lib

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 use {
-    actix_web::{HttpResponse, HttpRequest, error, post, web, App, HttpServer, Responder},
+    actix_web::{error, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder},
+    crossbeam_channel::{unbounded, RecvTimeoutError, Sender},
     derive_more::{Display, Error},
-    std::{time::Duration, collections::HashMap},
-    crossbeam_channel::{Sender, unbounded, RecvTimeoutError},
+    libloading::{Library, Symbol},
     log::*,
+    std::{
+        collections::HashMap,
+        ffi::{c_char, c_int, CString},
+        time::Duration,
+    },
 };
+
+type RunLlmFunc = unsafe fn(argc: c_int, argv: *const *const c_char) -> c_int;
 
 #[derive(Debug, Display, Error)]
 #[display(fmt = "my error: {}", name)]
@@ -29,10 +36,7 @@ async fn add_job(req: HttpRequest) -> Result<&'static str, MyError> {
 
 pub type JobData = HashMap<String, String>;
 
-async fn handle_request(
-    body: web::Bytes,
-    tx: web::Data<Sender<JobData>>
-) -> HttpResponse {
+async fn handle_request(body: web::Bytes, tx: web::Data<Sender<JobData>>) -> HttpResponse {
     info!("received req: {:?}", body);
     if let Ok(json) = serde_json::from_slice::<JobData>(&body) {
         //TODO: validate parameters
@@ -43,36 +47,60 @@ async fn handle_request(
     HttpResponse::NoContent().into()
 }
 
+fn handle_job(library: &Option<Library>) -> std::io::Result<()> {
+    if let Some(ref l) = library {
+        let model = "./models/7B/ggml-model-q4_0.bin";
+        let prompt = "give me a website";
+        let seed = "12345";
+        let args = format!("-m {} -p {} -s {}", model, prompt, seed);
+        let args = [CString::new("-c")?.as_ptr()];
+        unsafe {
+            let func_run_llm: Symbol<RunLlmFunc> = l.get(b"run_llm").unwrap();
+            func_run_llm(1, args.as_ptr());
+        }
+    }
+    Ok(())
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init();
-    let (sender, receiver) = unbounded::<()>();
-    let (tx, rx) = unbounded::<Vec<u8>>();
-    let worker = std::thread::Builder::new().spawn(move || {
-        loop {
+    let (tx, rx) = unbounded::<JobData>();
+    let library = unsafe {
+        match Library::new("libllama.dylib") {
+            Ok(l) => {
+                info!("loaded libllama! {:?}", l);
+                Some(l)
+            }
+            Err(e) => {
+                info!("loading error: {:?}", e);
+                None
+            }
+        }
+    };
+    let worker = std::thread::Builder::new()
+        .spawn(move || loop {
             match rx.recv_timeout(Duration::from_secs(1)) {
                 Ok(p) => {
                     info!("received a job: {:?}", p);
-                },
+                    handle_job(&library);
+                }
                 Err(RecvTimeoutError::Disconnected) => {
                     break;
-                },
-                Err(RecvTimeoutError::Timeout) => {
-                },
+                }
+                Err(RecvTimeoutError::Timeout) => {}
                 Err(e) => {
                     info!("error: {:?}", e);
                 }
             }
-        }
-    }).unwrap();
-    let sender = web::Data::new(sender);
-
+        })
+        .unwrap();
 
     HttpServer::new(move || {
         App::new()
             .data(tx.clone())
-            .service(web::scope("/api")
-                .route("/addjob", web::post().to(handle_request)))  // Set the endpoint to /addjob
+            .service(web::scope("/api").route("/addjob", web::post().to(handle_request)))
+        // Set the endpoint to /addjob
     })
     /*HttpServer::new(move || {
         App::new()
@@ -85,5 +113,3 @@ async fn main() -> std::io::Result<()> {
     .run()
     .await
 }
-
-
